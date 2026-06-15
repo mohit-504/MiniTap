f786d8f63de729fa7fb7e1cd5b36ffc051e6e940
mohit-504 <mohitmgokhale@gmail.com>
2026-06-15 11:04:28 +0530

    Added event metrics.

---
+++ src/main/java/project/controller/EventCollectorService.java
@@ -0,0 +1,36 @@
+// EventCollectorService.java
+package project.controller;
+
+import project.model.Event;
+import project.router.EventRouter;
+import project.util.EventMetrics;
+import project.util.Profiler;
+import project.util.TenantResolver;
+import project.validation.EventValidator;
+
+public class EventCollectorService {
+    private final EventValidator validator;
+    private final TenantResolver tenantResolver;
+    private final EventRouter router;
+    private final EventMetrics metrics;
+
+    public EventCollectorService(EventValidator validator, TenantResolver tenantResolver, EventRouter router, EventMetrics metrics) {
+        this.validator = validator;
+        this.tenantResolver = tenantResolver;
+        this.router = router;
+        this.metrics = metrics;
+    }
+
+    public void collect(Event event) {
+
+        Profiler profiler = new Profiler();
+        profiler.start();
+
+        validator.validate(event);
+        tenantResolver.resolve(event.tenantId());
+        router.route(event);
+        metrics.increment(event.eventType());
+
+        profiler.stop("Event Processing");
+    }
+}
---
+++ src/main/java/project/util/EventMetrics.java
@@ -0,0 +1,39 @@
+// EventMetrics.java 
+package project.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import project.model.EventType;
+
+public class EventMetrics {
+    private final Map<EventType, AtomicLong> counters = new ConcurrentHashMap<>();
+
+    public EventMetrics() {
+        for (EventType type : EventType.values()) {
+            counters.put(type, new AtomicLong());
+        }
+    }
+
+    public void increment(EventType eventType) {
+        counters.get(eventType).incrementAndGet();
+    }
+
+    public long getCount(EventType eventType) {
+        return counters.get(eventType).get();
+    }
+
+    public long getTotalCount() {
+        return counters.values()
+                .stream()
+                .mapToLong(AtomicLong::get)
+                .sum();
+    }
+
+    public void printMetrics() {
+        counters.forEach((type, count) -> System.out.println(type + " -> " + count.get()));
+
+        System.out.println("TOTAL -> " + getTotalCount());
+    }
+}
